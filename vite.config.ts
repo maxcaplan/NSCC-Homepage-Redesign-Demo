00840385c502148a90496f88678fd8365c0a30ea
maxcaplan <maxacaplan@gmail.com>
2025-11-29 10:59:31 -0400

    Updated vite config for app base env variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,36 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import preact from '@preact/preset-vite'
 import svgr from 'vite-plugin-svgr'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [
-		preact({
-			prerender: {
-				enabled: true,
-				renderTarget: "#app",
-				previewMiddlewareEnabled: true
-			}
-		}),
-		svgr(),
-		ViteImageOptimizer(),
-	],
-	build: {
-		cssMinify: "lightningcss",
-	},
-	resolve: {
-		alias: [
-			// Alias /src/ -> @
-			{
-				find: '@/',
-				replacement: fileURLToPath(new URL('/src/', import.meta.url))
-			},
-		]
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
+	return {
+		base: JSON.stringify(env.APP_BASE) || "/",
+		plugins: [
+			preact({
+				prerender: {
+					enabled: true,
+					renderTarget: "#app",
+					previewMiddlewareEnabled: true
+				}
+			}),
+			svgr(),
+			ViteImageOptimizer(),
+		],
+		build: {
+			cssMinify: "lightningcss",
+		},
+		resolve: {
+			alias: [
+				// Alias /src/ -> @
+				{
+					find: '@/',
+					replacement: fileURLToPath(new URL('/src/', import.meta.url))
+				},
+			]
+		}
 	}
 })
